a37ef4258e8019fefc4f9c911a3875fd8c79152d
Mark <igushkin.m@gmail.com>
2023-08-21 23:34:18 +0100

    21 aug

--- main-service/src/main/java/ru/practicum/ewm/base/config/StatsClientConfig.java
+++ main-service/src/main/java/ru/practicum/ewm/base/config/StatsClientConfig.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.base.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
