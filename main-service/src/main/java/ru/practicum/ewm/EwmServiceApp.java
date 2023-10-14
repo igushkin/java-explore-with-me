42d42ecc1e8be7c869f8b5e9ce49619d4303935f
Mark Igushkin <igushkin.m@yandex.ru>
2023-10-14 14:44:23 +0100

    update

--- main-service/src/main/java/ru/practicum/ewm/EwmServiceApp.java
+++ main-service/src/main/java/ru/practicum/ewm/EwmServiceApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EwmServiceApp {
-
     public static void main(String[] args) {
         SpringApplication.run(EwmServiceApp.class, args);
     }
